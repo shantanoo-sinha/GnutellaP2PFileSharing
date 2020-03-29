ecf4b5137c7e8ce8b1faba3144266cc370b8b5f5
Shantanoo Sinha <shantanoo.sinha@gmail.com>
2020-03-28 19:08:45 -0500

    Configuration for Performance testing

---
+++ src/test/resources/Clients/Client22/Client22-linear.bat
@@ -0,0 +1,4 @@
+set path = "C:\Program Files\Java\jdk1.8.0_151\bin"
+timeout /t 5
+java -classpath "F:\Workspace\GnutellaP2PFileSharing\target\GnutellaP2PFileSharing\lib\classes;F:\Workspace\GnutellaP2PFileSharing\target\GnutellaP2PFileSharing\lib\*" -Dlog4j.configurationFile=file:///f:/Workspace/GnutellaP2PFileSharing/target/GnutellaP2PFileSharing/lib/classes/log4j2.properties -Djava.rmi.server.codebase=file:f:/Workspace/GnutellaP2PFileSharing/target/GnutellaP2PFileSharing/lib/classes -Djava.security.policy=file:///f:/Workspace/GnutellaP2PFileSharing/target/GnutellaP2PFileSharing/lib/classes/security.policy client.Client linear Client22
+timeout /t 50
